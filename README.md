c65fa00ea9d5b4941b967670eccb6991d8278dab
rullyindraa <rullyindraa@gmail.com>
2018-07-12 15:28:03 +0700

    edit html and img assets

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# slicing-ui
+# Slicing TechId UI
+
+This project is about slicing TechId UI into HTML using Tailwind CSS Framework.
+
+## Installation
+
+- Clone the Repository.
+- Run `npm install`.
+- Run `gulp build`.
+- Run `gulp server`.
+- Run `http://localhost:8080/`in your browser.
--- gulpfile.js
+++ gulpfile.js
@@ -5,28 +5,28 @@ const uglify = require('gulp-uglify');
 const htmlmin = require('gulp-htmlmin');
 const connect = require('gulp-connect');
 const imagemin = require('gulp-imagemin');
+const sequence = require('gulp-sequence');
 
 gulp.task('minify-css', function() {
-  gulp.src('./public/assets/css/style.css')
-  .pipe(concat('style.min.js'))
-  .pipe(minify({
-    compatibility: 'ie8'
-  }))
-  .pipe(gulp.dest('./build'));
+  gulp
+    .src('./public/assets/css/style.css')
+    .pipe(concat('style.css'))
+    .pipe(minify({
+      compatibility: 'ie8'
+    }))
+    .pipe(gulp.dest('./build/public/assets/css'));
 });
 
 gulp.task('minify-js', function() {
   gulp
-    .src([
-      './public/assets/javascripts/script.js'
-    ])
-    .pipe(concat('script.min.js'))
+    .src('./public/assets/javascripts/script.js')
+    .pipe(concat('script.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./build'));
+    .pipe(gulp.dest('./build/public/assets/javascripts/'));
 });
 
 gulp.task('minify-html', function() {
-  gulp.src('./views/templates/index.html')
+  gulp.src('./views/index.html')
     .pipe(concat('index.html'))
     .pipe(htmlmin({
       collapseWhitespace: true
@@ -38,7 +38,7 @@ gulp.task('imagemin', function() {
   gulp
     .src('./public/assets/img/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('./img'))
+    .pipe(gulp.dest('./build/public/assets/img'))
 });
 
 gulp.task('server', function() {
@@ -54,3 +54,5 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', ['watch', 'server']);
+
+gulp.task('build', sequence('minify-css', 'minify-js', 'minify-html', 'imagemin'));
